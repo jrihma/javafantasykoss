f8945d1eed43f5fc8ff00dcb4c7acefab30ecac4
Joonas Rihma <jrihma@itcollege.ee>
2016-10-28 23:51:25 +0300

    võit vs kaotus

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,19 @@
 public class Main {
     public static void main(String args[]) {
         System.out.println("NBA Fantasy - head match-up'i!");
+
+        int mina = 30;
+        int vastane = 32;
+
+        mina = mina - vastane;
+
+        if (mina > 0) {
+            System.out.println("võit!");
+        }  else {
+            System.out.println("kaotus!");
+        }
     }
+
+
+
 }
